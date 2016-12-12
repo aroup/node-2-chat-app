e0ff4be36a6f86997c3b665528ed8783a11bbdd7
Aroup Goldar <aroupdhruba@gmail.com>
2016-12-12 21:32:23 +0600

    Initialized Directory

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+
+const port = process.env.PORT || 3000 ;
+
+app.use(express.static(publicPath));
+
+
+app.listen(port,()=>{
+  console.log(`Socket IO is running in ${port}`);
+});
