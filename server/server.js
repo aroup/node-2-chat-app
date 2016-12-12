2e839076bcb37a2aa9e35d25474ee16a8f81fd91
Aroup Goldar <aroupdhruba@gmail.com>
2016-12-12 22:47:12 +0600

    Including Socket.io clientside and severside

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 var express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 var app = express();
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
-
+var server = http.createServer(app);
+var io = socketIO(server);
 const port = process.env.PORT || 3000 ;
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+  console.log('New user Connected!');
+  socket.on('disconnect',()=>{
+    console.log('Disconnected!');
+  })
+});
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`Socket IO is running in ${port}`);
 });
