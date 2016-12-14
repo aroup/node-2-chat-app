0978ff5553292ba37ae3bcab1725d32564ece066
Aroup Goldar <aroupdhruba@gmail.com>
2016-12-14 20:19:22 +0600

    Add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,26 @@ socket.on('connect',function (){
 
 socket.on('newMessage',function(message){
   console.log('new message ',message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from} : ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.on('disconnect',function(){
   console.log('disconnected!');
 });
 
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from :'User',
+    text : jQuery('[name=message]').val()
+  },function(){
+
+  })
+});
 // socket.on('newEmail',function(email){
 //   console.log('new email',email);
 // });
--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ io.on('connection',(socket)=>{
   //   console.log('createEmail',newEmail);
   // });
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message,callback)=>{
     console.log('Created Message ',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('This is from the server!');
     // io.emit('newMessage',{
     //   from : message.from,
     //   text : message.text,
