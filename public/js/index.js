e59f064c865de44008ca9487eb1536dfc84b58c1
Aroup Goldar <aroupdhruba@gmail.com>
2016-12-15 19:02:07 +0600

    Added autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,16 @@
 var socket = io();
+function scrollToBottom (){
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop= messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
 socket.on('connect',function (){
   console.log('connected to server!');
 });
@@ -12,6 +24,7 @@ socket.on('newMessage',function(message){
     createdAt : formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
