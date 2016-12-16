236e36fcd2fcfcc2248947b0af693e33ed039441
Aroup Goldar <aroupdhruba@gmail.com>
2016-12-16 15:36:08 +0600

    Add User class and update user list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,16 @@ function scrollToBottom (){
   }
 }
 socket.on('connect',function (){
+  var params = jQuery.deparam(window.location.search);
+  socket.emit('join',params,function(err){
+    if(err){
+      alert(err);
+      window.location.href='/';
+    }
+    else {
+      console.log('No error!');
+    }
+  });
   console.log('connected to server!');
 });
 
@@ -25,6 +35,15 @@ socket.on('newMessage',function(message){
   });
   jQuery('#messages').append(html);
   scrollToBottom();
+});
+
+socket.on('updateUserList',function(users){
+  console.log('Users list',users);
+  var ol = jQuery('<ol></ol>');
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user))
+  });
+  jQuery('#users').html(ol);
 });
 
 socket.on('newLocationMessage',function(message){
--- server/server.js
+++ server/server.js
@@ -9,26 +9,46 @@ var io = socketIO(server);
 const port = process.env.PORT || 3000 ;
 const {generateMessage} = require('./utils/message');
 const {generateLocationMessage} = require('./utils/locationMessage');
-
+const {isRealString}= require('./utils/validation');
+const {Users} = require('./utils/users');
 app.use(express.static(publicPath));
+var users=new Users();
 
 io.on('connection',(socket)=>{
   console.log('New user Connected!');
-  socket.emit('newMessage',generateMessage('Admin','Welcome to the Chat app'));
-  socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+  socket.on('join',(params,callback)=>{
+    if(!isRealString(params.name) || !isRealString(params.room)){
+      return callback('Name and Room name are required');
+    }
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id,params.name,params.room);
+    io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+    //io.emit -> connects to everysingle connected user
+    //socket.broadcast.emit-> message to everyone connected except current user
+    //socket.emit -> emits message to a single user
+
+    socket.emit('newMessage',generateMessage('Admin','Welcome to the Chat app'));
+    socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} joined`));
+    callback();
+  });
 
   socket.on('createMessage',(message,callback)=>{
     console.log('Created Message ',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
     callback();
-
   });
+
   socket.on('createLocationMessage',(coords)=>{
     io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude , coords.longitude))
   });
 
   socket.on('disconnect',()=>{
-    console.log('Disconnected!');
+    var user = users.removeUser(socket.id);
+    if(user){
+      io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+      io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left`));
+    }
   });
 });
 
